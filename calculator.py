27f881eb260c92c58604c8669ab65e0f69df315b
Ludmila3515 <sobolevaludmila2005@gmail.com>
2024-12-13 14:30:31 +0300

    Операция возведения в степень

--- calculator.py
+++ calculator.py
@@ -9,17 +9,29 @@ def multiply(x, y):
 
 
 
+
+
+def power(x, y):
+    return x ** y
+
+
+
 # Эта программа всего лишь учебный пример
 print("Выберите операцию:")
 print("1. Сложение")
 print("2. Вычитание")
 print("3. Умножение")
 
+print("5. Возведение в степень")
+
+
 
 while True:
-    choice = input("Введите номер операции (1/2/3/4): ")
 
-    if choice in ['1', '2', '3', '4']:
+    choice = input("Введите номер операции (1/2/3/4/5): ")
+
+
+    if choice in ['1', '2', '3', '4','5']:
         num1 = float(input("Введите первое число: "))
         num2 = float(input("Введите второе число: "))
 
@@ -29,6 +41,12 @@ while True:
             print(f"{num1} - {num2} = {subtract(num1, num2)}")
         elif choice == '3':
             print(f"{num1} * {num2} = {multiply(num1, num2)}")
+
+
+
+        elif choice == '5':
+            print(f"{num1} в степени {num2} = {power(num1, num2)}")
+            
         
 
         # Спрашиваем пользователя, хочет ли он продолжить
@@ -36,4 +54,5 @@ while True:
         if next_calculation.lower() != 'да':
             break
     else:
-        print("Неверный ввод. Пожалуйста, выберите номер операции от 1 до 4.")
+        
+        print("Неверный ввод. Пожалуйста, выберите номер операции от 1 до 5.")
